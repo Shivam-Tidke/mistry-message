d2c16706cc1bd12f69ab9d5616bb94fea5e7e55e
Shivam-Tidke <shivamtidke2000@gmail.com>
2026-01-03 12:20:41 +0530

    feat schemas with zod library

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,13 @@
+import {email, z} from "zod"
+
+export const usernameValidation = z
+.string()
+.min(2,"username must be atleast 2 characters")
+.max(20, "username must be no more than 20 characters")
+.regex(/^[a-zA-Z0-9_]+$/, "username must not contain spacial characters")
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.email("Please enter a valid email address"),
+    password: z.string().min(6, {message:'Password must be at least 6 characters'})
+})    
